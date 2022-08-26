298e6d0db6945023c4cb9bda875b99e622cb5225
Arslan <harslanhafeez@gmail.com>
2022-08-26 22:55:30 +0500

    changes

--- js/myscript.js
+++ js/myscript.js
@@ -1,24 +1,71 @@
-function saveService(){
+var services = [];
+function saveService() {
     var serviceName = document.getElementById('serviceName').value;
     var supplier = document.getElementById('supplier').value;
     var purchasePrice = document.getElementById('purchasePrice').value;
     var sellingPrice = document.getElementById('sellingPrice').value;
     var serviceType = document.querySelector("input[type='radio'][name=serviceType]:checked").value;
-    var service = {
-        serviceName: serviceName,
-        supplier: supplier, 
-        purchasePrice: purchasePrice,
-        sellingPrice: sellingPrice,
-        serviceType: serviceType
-    }
-    
-    console.log(service);
-    
+
     // calculating profit
-    if (service.serviceType == 'true') {
-        var profit = (service.purchasePrice * 12 ) - service.sellingPrice;
+    if (serviceType == 'true') {
+        var profit = (purchasePrice * 12) - sellingPrice;
     } else {
-        var profit = service.sellingPrice - service.purchasePrice;
+        var profit = sellingPrice - purchasePrice;
     }
     document.getElementById('profit').value = profit;
+
+    var service = {
+        serviceName: serviceName,
+        supplier: supplier,
+        purchasePrice: purchasePrice,
+        sellingPrice: sellingPrice,
+        serviceType: serviceType,
+        profit: profit
+    }
+    services.push(service);
+
+    console.log(services);
 }
+
+
+// array operations
+var employess = [
+    {
+        id: 123,
+        name: 'rana',
+        email: 'rana@gmail.com',
+    },
+    {
+        id: 124,
+        name: 'arsal',
+        email: 'arsal@gmail.com',
+    },
+    {
+        id: 125,
+        name: 'shan',
+        email: 'shan@gmail.com',
+    },
+    {
+        id: 126,
+        name: 'ali',
+        email: 'ali@gmail.com',
+    }
+];
+employess.push(
+    {
+    id: 127,
+    name: 'jane',
+    email: 'jane@gmail.com',
+    }
+)
+employess.push(
+    {
+    id: 127,
+    name: 'jane',
+    email: 'jane@gmail.com',
+    }
+)
+
+employess.map(function(item, index) { 
+    console.log(item, index)
+});
